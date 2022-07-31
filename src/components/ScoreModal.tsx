840985ffd78bd9de466231ec8c073e215de111f9
moshe vilner <shootermv@gmail.com>
2022-07-31 12:35:24 +0300

    refactor: modal moved to ScroreModal component

---
+++ src/components/ScoreModal.tsx
@@ -0,0 +1,74 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {View, Text, StyleSheet, Modal, Pressable} from 'react-native';
+const ScoreModal = ({
+  setModalVisible,
+  modalVisible,
+}: {
+  setModalVisible: Function;
+  modalVisible: boolean;
+}) => {
+  const navigation = useNavigation();
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        console.log('Modal has been closed.');
+        setModalVisible(!modalVisible);
+      }}>
+      <View>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Hello World!</Text>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => {
+              setModalVisible(!modalVisible);
+              navigation.navigate('Scores');
+            }}>
+            <Text style={styles.textStyle}>Save Score</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+const styles = StyleSheet.create({
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+export default ScoreModal;
--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,62 +1,28 @@
-import React, { useState } from 'react';
-import {Modal, StyleSheet, Text, Pressable, View} from 'react-native';
-const Game = ({navigation}) => {
+import React, {useState} from 'react';
+import {StyleSheet, Text, Pressable, View} from 'react-native';
+import ScoreModal from '../components/ScoreModal';
+const Game = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+
   return (
     <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          console.log('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => { 
-                setModalVisible(!modalVisible);
-                navigation.navigate('Scores');
-              }}>
-              <Text style={styles.textStyle}>Save Score</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+      <ScoreModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
       <Pressable
         style={[styles.button, styles.buttonOpen]}
         onPress={() => setModalVisible(true)}>
-        <Text style={styles.textStyle}>Show Modal</Text>
+        <Text style={styles.textStyle}>
+          {gameStarted ? 'Game Started' : 'Start Game'}
+        </Text>
       </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
   button: {
     borderRadius: 20,
     padding: 10,
@@ -65,16 +31,15 @@ const styles = StyleSheet.create({
   buttonOpen: {
     backgroundColor: '#F194FF',
   },
-  buttonClose: {
-    backgroundColor: '#2196F3',
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
     textAlign: 'center',
   },
 });
