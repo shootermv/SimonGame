bbbaa4981a778a7c4d818ee4b55bc9f0cc244715
moshe vilner <shootermv@gmail.com>
2022-08-01 09:56:51 +0300

    feat: make store persistent

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import Main from './navigation/Main';
-import {store} from './store';
+import {store, persistor} from './store';
 import {Provider} from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react';
 export default function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <NavigationContainer>
         <Main />
-      </NavigationContainer>
+      </NavigationContainer></PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import scoresReducer from './scoresSlice';
 import gameReducer from './gameSlice';
+import {persistReducer, persistStore} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+
+const rootReducer = combineReducers({
+  scores: scoresReducer,
+  game: gameReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    scores: scoresReducer,
-    game: gameReducer,
-  },
-})
+  reducer: persistedReducer,
+});
+export const persistor = persistStore(store)
